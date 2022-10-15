1f67c79405b3de54888e25d4f6adc436067ce3d2
sookyeong <sookyeong.dev@gmail.com>
2022-10-16 03:17:14 +0900

    Feature/default setting (#1)
    * chore: 필요없는 파일 제거
    
    * chore: 포맷터 설정
    
    * chore: styled-components 설치 및 설정 (#ERWT-146)
    
    * chore: scss 설치 및 설정 (#ERWT-146)
    
    * chore: reset css 적용 (#ERWT-146)
    
    * chore: 린터 설정 (#ERWT-146)
    
    * chore: react hook form 설치 및 styled-components devDep에서 dep으로 변경 (#ERWT-146)
    
    * chore: axios 설치 (#ERWT-146)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
+	},
+};
+
+module.exports = nextConfig;
