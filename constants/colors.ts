d723b14543f14c4ea26a7efcd76fbec887728c43
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-05 11:36:15 +0900

    Feat: 와이어프레임 색상 상수 정의

---
+++ constants/colors.ts
@@ -0,0 +1,24 @@
+export default class Colors {
+	static white01 = '#FFFFFF';
+
+	static black01 = '#141618';
+	static black02 = '#4D4E54';
+	static black03 = '#808289';
+	static black04 = '#B0B0B0';
+
+	static background01 = '#EFF2F4';
+	static background02 = '#F3F4F4';
+	static background03 = '#D2D2D2';
+	static background04 = '#8E8E8E';
+	static background05 = '#CACACA';
+
+	static border01 = '#DEDBDB';
+	static border02 = '#EAEAEA';
+	static border03 = '#BDBDBD';
+	static border04 = '#DFE2E5';
+	static border05 = '#D0CFCF';
+
+	static highlight01 = '#7E9DEE';
+
+	static placeholder01 = '#B9C0C6';
+}
