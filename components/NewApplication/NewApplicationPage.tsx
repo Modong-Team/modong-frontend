e8c6fc1dffde0cf5839fd4e5cb918defdab03927
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-07 16:13:21 +0900

    Feat: 지원서 필수 항목 선택 여부를 최상위 컴포넌트에서 관리

--- components/NewApplication/NewApplicationPage.tsx
+++ components/NewApplication/NewApplicationPage.tsx
@@ -1,14 +1,30 @@
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import NewApplicationContent from './NewApplicationContent';
 import NewApplicationLayout from './NewApplicationLayout';
 import NewApplicationTitle from './NewApplicationTitle';
+import { postApplication, patchApplication } from '../../api/application';
 
 export default function NewApplicationPage() {
+	const titleRef = useRef<HTMLInputElement>(null);
+	const [essentials, setEssentials] = useState([1, 2, 3, 4, 5, 6]);
+
+	const onDone = async () => {
+		const title = titleRef.current!.value;
+		const post = await postApplication(1, title)
+			.then(async (res) => {
+				console.log(res);
+				const id = res.data.id;
+				const patch = await patchApplication(id, essentials).then(console.log);
+			})
+			.catch(console.log);
+	};
+
 	return (
-		<NewApplicationLayout>
+		<NewApplicationLayout onDone={onDone}>
 			<NewApplicationContainer>
-				<NewApplicationTitle />
-				<NewApplicationContent />
+				<NewApplicationTitle titleRef={titleRef} />
+				<NewApplicationContent essentials={essentials} setEssentials={setEssentials} />
 			</NewApplicationContainer>
 		</NewApplicationLayout>
 	);
