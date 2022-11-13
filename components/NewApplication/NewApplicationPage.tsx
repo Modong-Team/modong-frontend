90ee7dd9c31d0314b5a6c155f29aec01a6197a6d
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-14 04:41:57 +0900

    Feat: 섹션 이동 및 추가 로직 개발

--- components/NewApplication/NewApplicationPage.tsx
+++ components/NewApplication/NewApplicationPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import NewApplicationContent from './NewApplicationContent';
 import NewApplicationLayout from './NewApplicationLayout';
@@ -7,10 +7,19 @@ import { postApplication, patchApplication } from '../../api/application';
 import NewApplicationIndicator from './NewApplicationIndicator';
 import NewApplicationNavigator from './NewApplicationNavigator';
 import NewApplicationButton from './NewApplicationButton';
+import { DummySections } from '../../models/sections';
 
 export default function NewApplicationPage() {
 	const titleRef = useRef<HTMLInputElement>(null);
 	const [essentials, setEssentials] = useState([1, 2, 3]);
+	const [currentSection, setCurrentSection] = useState(0);
+	const [sections, setSections] = useState([...DummySections]);
+	const [sectionsLength, setSectionsLength] = useState(0);
+	const [removedSections, setRemovedSections] = useState([]);
+
+	useEffect(() => {
+		setSectionsLength(sections.length);
+	}, [sections]);
 
 	const onDone = async () => {
 		const title = titleRef.current!.value;
@@ -24,14 +33,42 @@ export default function NewApplicationPage() {
 			.catch(console.log);
 	};
 
+	const onNext = () => {
+		if (currentSection === sections.length - 1)
+			setSections([...sections, { title: '질문 페이지' }]);
+		setCurrentSection(currentSection + 1);
+	};
+
+	const onPrev = () => {
+		setCurrentSection(currentSection - 1);
+	};
+
+	const onRouteToSection = (idx: number) => {
+		setCurrentSection(idx);
+	};
+
 	return (
 		<NewApplicationLayout onDone={onDone}>
 			<NewApplicationContainer>
 				<NewApplicationTitle titleRef={titleRef} />
 				<NewApplicationIndicator />
-				<NewApplicationContent essentials={essentials} setEssentials={setEssentials} />
-				<NewApplicationNavigator />
-				<NewApplicationButton />
+				<NewApplicationContent
+					essentials={essentials}
+					setEssentials={setEssentials}
+					currentSection={currentSection}
+					section={sections[currentSection]}
+				/>
+				<NewApplicationNavigator
+					sections={sections}
+					currentSection={currentSection}
+					onRouteToSection={onRouteToSection}
+				/>
+				<NewApplicationButton
+					currentSection={currentSection}
+					sectionsLength={sectionsLength}
+					onNext={onNext}
+					onPrev={onPrev}
+				/>
 			</NewApplicationContainer>
 		</NewApplicationLayout>
 	);
