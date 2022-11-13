c24e82af11985a54df1494c65d3a5df66f5137d5
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-14 07:23:41 +0900

    Feat: Default 질문 추가 모달 퍼블리싱

--- components/NewApplication/NewApplicationDefault.tsx
+++ components/NewApplication/NewApplicationDefault.tsx
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 import Colors from '../../constants/colors';
 import Fonts from '../../constants/fonts';
-import { svgCirclePlus, svgCircleX } from '../../constants/svgs';
+import { svgCirclePlus, svgCircleX, svgPencil, svgTick, svgCheckBox } from '../../constants/svgs';
 import { NewApplicationDefaultProps } from './propsTypes';
+import { useState } from 'react';
 
 export default function NewApplicationDefault({ section }: NewApplicationDefaultProps) {
+	const [showMenu, setShowMenu] = useState(false);
+
+	const onClickMenu = () => setShowMenu(true);
+	const onBlur = () => setShowMenu(false);
+
 	return (
 		<>
-			<h2>{section.title}</h2>
+			<h2>{section && section.title}</h2>
 			<p>지원자에게 질문하고 싶은 내용을 입력해주세요.</p>
 			<DefaultContainer>
 				<DefaultInputContainer>
@@ -17,7 +23,16 @@ export default function NewApplicationDefault({ section }: NewApplicationDefault
 						{svgCircleX}
 					</DefaultInputText>
 				</DefaultInputContainer>
-				<span>{svgCirclePlus}</span>
+				<NewQuestionButton onClick={onClickMenu} onBlur={onBlur}>
+					{svgCirclePlus}
+					{showMenu && (
+						<QuestionMenu>
+							<div>{svgPencil}주관식</div>
+							<div>{svgTick}단일 선택</div>
+							<div>{svgCheckBox}복수 선택</div>
+						</QuestionMenu>
+					)}
+				</NewQuestionButton>
 			</DefaultContainer>
 		</>
 	);
@@ -72,6 +87,43 @@ const DefaultInputText = styled.div`
 		}
 		svg {
 			margin-bottom: 0.6rem;
+		}
+	}
+`;
+
+const NewQuestionButton = styled.button`
+	position: relative;
+	text-align: center;
+	width: fit-content;
+	margin: 0 auto;
+`;
+
+const QuestionMenu = styled.div`
+	white-space: nowrap;
+	position: absolute;
+	bottom: -10rem;
+	left: 50%;
+	transform: translateX(-50%);
+	text-align: left;
+	background-color: ${Colors.white};
+	border: 0.1rem solid ${Colors.gray200};
+	border-radius: 0.4rem;
+	padding: 0.25rem 0;
+	box-shadow: 6px 7px 16px rgba(106, 106, 106, 0.17);
+
+	& {
+		div {
+			${Fonts.subtitle14semibold}
+			padding:0.35rem 1.35rem;
+			transition: 0.3s ease;
+			display: flex;
+			align-items: center;
+			gap: 0.65rem;
+
+			&:hover {
+				background-color: ${Colors.gray100};
+				transition: 0.3s ease;
+			}
 		}
 	}
 `;
