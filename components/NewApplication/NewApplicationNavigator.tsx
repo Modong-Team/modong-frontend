46c44d0fa86317aa631d392b2d37b9dcdc8e9922
sookyeong <sookyeong.dev@gmail.com>
2022-11-14 07:38:54 +0900

    Feat: Navigator 더보기 모달 (#11)

--- components/NewApplication/NewApplicationNavigator.tsx
+++ components/NewApplication/NewApplicationNavigator.tsx
@@ -3,18 +3,37 @@ import Colors from '../../constants/colors';
 import { NewApplicationNavigatorProps } from './propsTypes';
 import { NavigatorElementProps } from './styledTypes';
 import { svgVertical } from '../../constants/svgs';
+import Fonts from '../../constants/fonts';
+import { useState } from 'react';
 
 export default function NewApplicationNavigator({
 	sections,
 	currentSection,
 	onRouteToSection,
+	onRemove,
 }: NewApplicationNavigatorProps) {
+	const [showMoreFor, setShowMoreFor] = useState(-1);
+
+	const onClickMore = (idx: number) => setShowMoreFor(idx);
+	const onBlur = () => setShowMoreFor(-1);
+
+	const onClickRemove = (idx: number) => {
+		setShowMoreFor(-1);
+		onRemove(idx);
+	};
+
 	return (
 		<NavigatorContainer>
 			{sections.map((v, i) => (
-				<NavigatorElement isCurrent={i === currentSection} key={i}>
+				<NavigatorElement isCurrent={i === currentSection} key={i} onBlur={onBlur}>
 					<span onClick={() => onRouteToSection(i)}>{v.title}</span>
-					{i !== 0 && svgVertical}
+					<span onClick={() => onClickMore(i)}>{i !== 0 && svgVertical}</span>
+					{showMoreFor === i && (
+						<NavigatorMore>
+							<div onClick={() => onClickRemove(i)}>삭제하기</div>
+							<div>복제하기</div>
+						</NavigatorMore>
+					)}
 				</NavigatorElement>
 			))}
 		</NavigatorContainer>
@@ -27,23 +46,65 @@ const NavigatorContainer = styled.section`
 	flex-direction: column;
 	gap: 0.8rem;
 	grid-column: 3/4;
+	grid-row: 2/5;
 `;
 
-const NavigatorElement = styled.div<NavigatorElementProps>`
+const NavigatorElement = styled.button<NavigatorElementProps>`
+	box-sizing: border-box;
+	position: relative;
 	background-color: ${(props) => (props.isCurrent ? Colors.white : Colors.gray200)};
 	border: 0.1rem solid ${(props) => (props.isCurrent ? Colors.gray200 : Colors.gray300)};
 	color: ${(props) => (props.isCurrent ? Colors.gray900 : Colors.gray500)};
-	padding: 1.2rem 1.6rem;
+	padding: 0rem 1.6rem;
 	padding-right: 1rem;
 	border-radius: 0.8rem;
 	width: 17.4rem;
+	height: 4.8rem;
 	display: flex;
+	align-items: center;
 	justify-content: space-between;
 
 	& {
 		span,
 		svg {
 			cursor: pointer;
+		}
+
+		svg {
+			position: relative;
+			top: 0.1rem;
+		}
+	}
+`;
+
+const NavigatorMore = styled.div`
+	position: absolute;
+	bottom: -5.3rem;
+	right: -2.25rem;
+	background-color: ${Colors.white};
+	z-index: 10;
+	border: 0.1rem solid ${Colors.gray200};
+	border-radius: 0.4rem;
+	box-shadow: 6px 7px 16px rgba(106, 106, 106, 0.17);
+	transition: 0.3s ease;
+
+	& {
+		div {
+			${Fonts.subtitle14semibold}
+
+			&:hover {
+				background-color: ${Colors.gray100};
+				transition: 0.3s ease;
+			}
+		}
+
+		div:first-child {
+			color: ${Colors.red};
+			padding: 0.7rem 1.6rem 0.4rem 1.6rem;
+		}
+
+		div:last-child {
+			padding: 0.4rem 1.6rem 0.7rem 1.6rem;
 		}
 	}
 `;
