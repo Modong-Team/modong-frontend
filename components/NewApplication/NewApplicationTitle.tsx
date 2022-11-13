7c323eace511465286ac292e332b0e3c2b7ab8e7
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-13 22:29:53 +0900

    Feat: 제목 스타일 변경

--- components/NewApplication/NewApplicationTitle.tsx
+++ components/NewApplication/NewApplicationTitle.tsx
@@ -6,14 +6,14 @@ import { NewApplicationTitleProps } from './propsTypes';
 export default function NewApplicationTitle({ titleRef }: NewApplicationTitleProps) {
 	return (
 		<TitleContainer>
-			<input type='text' placeholder='지원서 제목을 입력하세요' ref={titleRef} />
+			<input type='text' placeholder='지원서 제목' ref={titleRef} />
 		</TitleContainer>
 	);
 }
 
 const TitleContainer = styled.div`
 	border-radius: 0.8rem;
-	padding: 1.6rem 2.4rem;
+	padding: 2.2rem 4rem;
 	background-color: ${Colors.white01};
 	border: 0.1rem solid ${Colors.border02};
 	grid-column: 2/3;
@@ -21,13 +21,12 @@ const TitleContainer = styled.div`
 	& {
 		input {
 			width: 100%;
-			border-bottom: 0.2rem solid ${Colors.border04};
-			padding: 1.2rem;
-			padding-bottom: 0.8rem;
-			font-weight: 700;
-			font-size: 3.2rem;
+			/* border-bottom: 0.2rem solid ${Colors.border04}; */
+			font-weight: 600;
+			font-size: 2.6rem;
 			color: ${Colors.black01};
 			caret-color: ${Colors.highlight01};
+			background-color: ${Colors.white01};
 
 			&::placeholder {
 				color: ${Colors.placeholder01};
