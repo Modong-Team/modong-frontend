37e551eab16286ba7246dbe7d50fb625a590df6e
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-13 16:38:06 +0900

    Feat: 와이어프레임 추가 색상 정의

--- constants/colors.ts
+++ constants/colors.ts
@@ -6,17 +6,21 @@ export default class Colors {
 	static black03 = '#808289';
 	static black04 = '#B0B0B0';
 
+	static gray01 = '#ADB3C1';
+
 	static background01 = '#EFF2F4';
 	static background02 = '#F3F4F4';
 	static background03 = '#D2D2D2';
 	static background04 = '#8E8E8E';
 	static background05 = '#CACACA';
+	static background06 = '#E4E7EE';
 
 	static border01 = '#DEDBDB';
 	static border02 = '#EAEAEA';
 	static border03 = '#BDBDBD';
 	static border04 = '#DFE2E5';
 	static border05 = '#D0CFCF';
+	static border06 = '#D6DAE4';
 
 	static highlight01 = '#7E9DEE';
 
