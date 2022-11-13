c9fa7be909edb9328408626d1c67335be8ca9821
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-14 04:44:19 +0900

    Feat: 하단 Button 현재 섹션에 따른 동적 문구 및 로직 연결

--- components/NewApplication/NewApplicationButton.tsx
+++ components/NewApplication/NewApplicationButton.tsx
@@ -2,12 +2,24 @@ import styled from 'styled-components';
 import Colors from '../../constants/colors';
 import Fonts from '../../constants/fonts';
 import { svgPrev, svgNext } from '../../constants/svgs';
+import { NewApplicationButtonProps } from './propsTypes';
+import { ButtonProps } from './styledTypes';
 
-export default function NewApplicationButton() {
+export default function NewApplicationButton({
+	currentSection,
+	sectionsLength,
+	onNext,
+	onPrev,
+}: NewApplicationButtonProps) {
 	return (
 		<ButtonContainer>
-			<Button>{svgPrev}이전</Button>
-			<Button>다음{svgNext}</Button>
+			<Button onClick={onPrev} isHidden={currentSection === 0}>
+				{svgPrev}이전
+			</Button>
+			<Button onClick={onNext}>
+				{currentSection === sectionsLength - 1 ? '페이지 추가' : '다음'}
+				{svgNext}
+			</Button>
 		</ButtonContainer>
 	);
 }
@@ -18,14 +30,15 @@ const ButtonContainer = styled.section`
 	justify-content: space-between;
 `;
 
-const Button = styled.button`
+const Button = styled.button<ButtonProps>`
 	${Fonts.button14bold}
 	padding: 1.4rem 2rem;
 	display: flex;
-	gap: 0.6rem;
+	gap: 0.48rem;
 	border-radius: 0.8rem;
 	border: 0.1rem solid ${Colors.gray200};
 	background-color: ${Colors.white};
+	visibility: ${(props) => (props.isHidden ? 'hidden' : '')};
 
 	& {
 		svg {
