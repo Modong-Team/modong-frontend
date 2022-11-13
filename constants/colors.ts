97e5f032b58eada214197ddd1987460c3b869043
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-13 22:30:29 +0900

    Feat: Color 재정의

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,28 +1,22 @@
 export default class Colors {
-	static white01 = '#FFFFFF';
+	static white = '#FFFFFF';
+	static red = '#DC746D';
 
-	static black01 = '#141618';
-	static black02 = '#4D4E54';
-	static black03 = '#808289';
-	static black04 = '#B0B0B0';
+	static gray100 = '#EDEEF3';
+	static gray200 = '#E4E7EE';
+	static gray300 = '#D6DAE4';
+	static gray400 = '#C0C4CF';
+	static gray500 = '#ADB3C1';
+	static gray600 = '#9DA4B5';
+	static gray700 = '#8D95A8';
+	static gray800 = '#6E768B';
+	static gray900 = '#505666';
+	static gray950 = '#383C47';
+	static gray990 = '#2A2D34';
 
-	static gray01 = '#ADB3C1';
-
-	static background01 = '#EFF2F4';
-	static background02 = '#F3F4F4';
-	static background03 = '#D2D2D2';
-	static background04 = '#8E8E8E';
-	static background05 = '#CACACA';
-	static background06 = '#E4E7EE';
-
-	static border01 = '#DEDBDB';
-	static border02 = '#EAEAEA';
-	static border03 = '#BDBDBD';
-	static border04 = '#DFE2E5';
-	static border05 = '#D0CFCF';
-	static border06 = '#D6DAE4';
-
-	static highlight01 = '#7E9DEE';
-
-	static placeholder01 = '#B9C0C6';
+	static blue100 = '#DAE6FF';
+	static blue300 = '#ABC8FF';
+	static blue500 = '#7DA9FF';
+	static blue700 = '#6894EB';
+	static blue900 = '#3F74DB';
 }
