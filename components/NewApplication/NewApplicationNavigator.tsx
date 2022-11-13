a6559a22497716984100f1e5805abccec6e36f99
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-14 04:42:42 +0900

    Feat: Navigator 현재 섹션 하이라이팅 및 섹션 이동 로직 연결

--- components/NewApplication/NewApplicationNavigator.tsx
+++ components/NewApplication/NewApplicationNavigator.tsx
@@ -1,11 +1,22 @@
 import styled from 'styled-components';
 import Colors from '../../constants/colors';
+import { NewApplicationNavigatorProps } from './propsTypes';
+import { NavigatorElementProps } from './styledTypes';
+import { svgVertical } from '../../constants/svgs';
 
-export default function NewApplicationNavigator() {
+export default function NewApplicationNavigator({
+	sections,
+	currentSection,
+	onRouteToSection,
+}: NewApplicationNavigatorProps) {
 	return (
 		<NavigatorContainer>
-			<NavigatorElement>지원자 정보</NavigatorElement>
-			<NavigatorElement>질문 페이지</NavigatorElement>
+			{sections.map((v, i) => (
+				<NavigatorElement isCurrent={i === currentSection} key={i}>
+					<span onClick={() => onRouteToSection(i)}>{v.title}</span>
+					{i !== 0 && svgVertical}
+				</NavigatorElement>
+			))}
 		</NavigatorContainer>
 	);
 }
@@ -18,11 +29,21 @@ const NavigatorContainer = styled.section`
 	grid-column: 3/4;
 `;
 
-const NavigatorElement = styled.div`
-	background-color: ${Colors.gray200};
-	border: 0.1rem solid ${Colors.gray300};
+const NavigatorElement = styled.div<NavigatorElementProps>`
+	background-color: ${(props) => (props.isCurrent ? Colors.white : Colors.gray200)};
+	border: 0.1rem solid ${(props) => (props.isCurrent ? Colors.gray200 : Colors.gray300)};
+	color: ${(props) => (props.isCurrent ? Colors.gray900 : Colors.gray500)};
 	padding: 1.2rem 1.6rem;
+	padding-right: 1rem;
 	border-radius: 0.8rem;
 	width: 17.4rem;
-	color: ${Colors.gray500};
+	display: flex;
+	justify-content: space-between;
+
+	& {
+		span,
+		svg {
+			cursor: pointer;
+		}
+	}
 `;
