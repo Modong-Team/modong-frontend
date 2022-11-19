1347aa5138f78fac0ffdf7b438767f787dc6ee00
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-19 18:47:24 +0900

    Refactor: 기존 코드를 컴포넌트로 교체

--- components/NewApplication/NewApplicationDefault.tsx
+++ components/NewApplication/NewApplicationDefault.tsx
@@ -7,6 +7,8 @@ import { NewApplicationDefaultProps } from './props';
 import { useState } from 'react';
 import useInput from '../../hooks/useInput';
 import SectionTitleInput from '../Input/SectionTitleInput';
+import QuestionInput from '../Input/QuestionInput';
+import { Questions } from '../../constants/questions';
 
 export default function NewApplicationDefault({ section }: NewApplicationDefaultProps) {
 	const [showMenu, setShowMenu] = useState(false);
@@ -26,13 +28,7 @@ export default function NewApplicationDefault({ section }: NewApplicationDefault
 			</h2>
 			<p>지원자에게 질문하고 싶은 내용을 입력해주세요.</p>
 			<DefaultContainer>
-				<DefaultInputContainer>
-					<h3>주관식</h3>
-					<DefaultInputText>
-						<input placeholder='질문 제목을 입력해주세요' />
-						{svgCircleX}
-					</DefaultInputText>
-				</DefaultInputContainer>
+				<QuestionInput questionType={Questions.TextQuestion} />
 				<NewQuestionButton onClick={onClickMenu} onBlur={onBlur}>
 					{svgCirclePlus}
 					{showMenu && (
@@ -56,47 +52,6 @@ const DefaultContainer = styled.div`
 	& {
 		> span {
 			margin: 0 auto;
-		}
-	}
-`;
-
-const DefaultInputContainer = styled.div`
-	padding: 2.3rem 2.8rem;
-	background-color: ${Colors.white};
-	border-radius: 0.8rem;
-	border: 0.1rem solid ${Colors.gray200};
-	margin-top: 2rem;
-
-	& {
-		h3 {
-			${Fonts.subtitle14semibold}
-			margin-bottom: 0.9rem;
-		}
-	}
-`;
-
-const DefaultInputText = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	gap: 1.15rem;
-	background-color: ${Colors.white};
-
-	& {
-		input {
-			${Fonts.body16regular}
-			width: 100%;
-			background-color: transparent;
-			padding: 1rem 0.8rem;
-			border: 0.1rem solid ${Colors.gray200};
-			border-radius: 0.4rem;
-		}
-		input::placeholder {
-			${Fonts.body16regular}
-			color: ${Colors.gray400};
-		}
-		svg {
-			margin-bottom: 0.6rem;
 		}
 	}
 `;
