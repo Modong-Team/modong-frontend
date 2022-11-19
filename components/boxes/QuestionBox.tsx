28893298a8e8b8370f0ea641d69b71f70c04be15
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-20 05:43:39 +0900

    Feat: QuestionBox에 Context 적용

--- components/boxes/QuestionBox.tsx
+++ components/boxes/QuestionBox.tsx
@@ -4,31 +4,73 @@ import Fonts from '../../constants/fonts';
 import QuestionInput from '../Inputs/QuestionInput';
 import QuestionRadioInput from '../Inputs/QuestionRadioInput';
 import { QuestionBoxProps } from './props';
-import { Questions } from '../../constants/questions';
+import { Questions, QuestionsLabel } from '../../constants/questions';
 import QuestionCheckboxInput from '../Inputs/QuestionCheckboxInput';
 import CustomButton from '../buttons/CustomButton';
 import { ButtonSizes, ButtonTypes } from '../../constants/buttons';
-import { svgCirclePlus } from '../../constants/svgs';
+import { svgCirclePlusSmall } from '../../constants/svgs';
+import { useFormsActions } from '../../contexts/FormsProviders';
 
-export default function QuestionBox({ questionType, isError }: QuestionBoxProps) {
+export default function QuestionBox({
+	questionType,
+	isError,
+	question,
+	formIdx,
+	questionIdx,
+}: QuestionBoxProps) {
+	const actions = useFormsActions();
+
+	const onChangeQuestionContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+		actions.updateQuestionContent(formIdx, questionIdx, e.target.value);
+	};
+
+	const onRemoveQuestion = () => {
+		actions.removeQuestion(formIdx, questionIdx);
+	};
+
+	const onCreateQuestionOption = () => {
+		actions.createQuestionOption(formIdx, questionIdx);
+	};
+
 	return (
 		<QuestionBoxContainer>
-			<h3>{questionType}</h3>
-			<QuestionInput isError={isError} />
+			<h3>{QuestionsLabel[questionType]}</h3>
+			<QuestionInput
+				isError={isError}
+				onChange={onChangeQuestionContent}
+				onRemove={onRemoveQuestion}
+				placeholder='질문을 입력하세요.'
+				value={question.content}
+			/>
 			{questionType !== Questions.TextQuestion ? <h3>선택항목</h3> : ''}
-			{questionType === Questions.RadioQuestion ? (
-				<QuestionRadioInput />
-			) : questionType === Questions.CheckboxQuestion ? (
-				<QuestionCheckboxInput />
-			) : (
-				''
-			)}
+			{questionType === Questions.RadioQuestion
+				? question.options.map((v, i) => (
+						<QuestionRadioInput
+							question={question}
+							formIdx={formIdx}
+							questionIdx={questionIdx}
+							optionIdx={i}
+							key={i}
+						/>
+				  ))
+				: questionType === Questions.CheckboxQuestion
+				? question.options.map((v, i) => (
+						<QuestionCheckboxInput
+							question={question}
+							formIdx={formIdx}
+							questionIdx={questionIdx}
+							optionIdx={i}
+							key={i}
+						/>
+				  ))
+				: ''}
 			{questionType !== Questions.TextQuestion ? (
 				<CustomButton
+					onClick={onCreateQuestionOption}
 					buttonSize={ButtonSizes.medium}
 					buttonType={ButtonTypes.line}
 					margin='0 0 0 3rem'>
-					{svgCirclePlus} 항목 추가
+					{svgCirclePlusSmall} 항목 추가
 				</CustomButton>
 			) : (
 				''
