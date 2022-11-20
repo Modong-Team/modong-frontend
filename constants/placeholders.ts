192e59aa6190fe9a4b6773ba965b07bf28c3fe61
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-20 22:16:43 +0900

    Feat: Placeholder constant

---
+++ constants/placeholders.ts
@@ -0,0 +1,3 @@
+export class Placeholders {
+	static question = '제목 없는 질문';
+}
--- models/emptyForm.ts
+++ models/emptyForm.ts
@@ -1,12 +1,14 @@
 import { FormType } from '../@types/client/data';
+import { Questions } from '../constants/questions';
+import { Placeholders } from '../constants/placeholders';
 
 export const EmptyForm: FormType = {
 	id: null,
 	title: '질문 페이지',
 	questions: [
 		{
-			type: 'TextQuestion',
-			content: '',
+			type: `${Questions.TextQuestion}`,
+			content: `${Placeholders.question}`,
 			options: [''],
 		},
 	],
