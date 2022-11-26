ae8789f3eec391701e849f78cf00b5ede5908fcc
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-26 12:20:50 +0900

    Feat: common styled separated

---
+++ constants/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import Colors from './colors';
+import { Styles } from './styles';
+
+export const HeaderContainer = styled.header`
+	background: ${Colors.white};
+	border-bottom: 0.1rem solid ${Colors.gray200};
+	height: ${Styles.headerHeight};
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 2.4rem;
+`;
+
+export const HeaderLogo = styled.h1``;
