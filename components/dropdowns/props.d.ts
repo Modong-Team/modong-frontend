32d74d9fa25f425dcd10c946273a1827113947ee
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-27 13:21:23 +0900

    Feat: dropdown component separated

---
+++ components/dropdowns/props.d.ts
@@ -0,0 +1,11 @@
+export interface DropDownProps {
+	svg1?: any;
+	svg2?: any;
+	svg3?: any;
+	option1: string;
+	option2: string;
+	option3?: string;
+	onClick1: (e: any) => void;
+	onClick2: (e: any) => void;
+	onClick3?: (e: any) => void;
+}
